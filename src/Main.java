63fe92efe7f9f227db5b9c74085a8ef2fbcc3c39
I538957 <Yannis.Reitemeier@sap.com>
2021-07-27 21:51:24 +0200

    Jannik POC

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     System.out.println("Yves ist cool");
     System.out.println("Yannis ist auch cool");
     System.out.println("Dominik ist am coolsten");
+    System.out.println("Janik ist kacke");
   }
 }
