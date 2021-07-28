381a11ad256fd3efbecbe47415628e065d6b03a8
BohniZerstoerer <dominik.baienfurt@gmail.com>
2021-07-28 12:38:48 +0200

    Test Commit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     System.out.println("Yannis ist auch cool");
     System.out.println("Dominik ist am coolsten");
     System.out.println("Janik ist kacke");
+    System.out.println("Maik ist halt in Konstanz");
   }
 }
