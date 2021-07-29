b7118891377cfce6f3076fa2a61794dbcdd6a95d
YvesKipfer <yves.kipfer@sap.com>
2021-07-29 17:56:49 +0200

    HAllooo

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,7 @@ public class Main {
     System.out.println("Yannis ist auch cool");
     System.out.println("Dominik ist am coolsten");
     System.out.println("Janik ist kacke");
-  System.out.println("Hallo ich bin der Yves und ich bin auch dabei");
-  System.out.println(12);
-  System.out.println("Test2");
-
+  
     GUI g = new GUI();
     Stage stage = new Stage();
     g.start(stage);
