79f1fcddbd2c8187737a4c89f39c25d1ed0ba7fa
Yannis Reitemeier <78709065+YannisReitemeier@users.noreply.github.com>
2021-08-03 17:28:20 +0200

    Added JavaFX libaries, addded the settings file.

--- src/GUI.java
+++ src/GUI.java
@@ -1,36 +1,31 @@
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.event.*;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
-    
-    public class GUI extends Application {
-        public static void main(String[] args) {
-            launch(args);
-        }
-        
-        @Override
-        public void start(Stage primaryStage) {
-            primaryStage.setTitle("Farming Simulator");
-            Button btn = new Button();
-            btn.setText("Press Button to enter");
-            btn.setOnAction(new EventHandler<ActionEvent>() {
-     
-                @Override
-                public void handle(ActionEvent event) {
-                    System.out.println("Farming Simulator");
-                }
-            });
-            
-            StackPane root = new StackPane();
-            root.getChildren().add(btn);
-            primaryStage.setScene(new Scene(root, 300, 250));
-            primaryStage.show();
-        }
 
+public class GUI extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
 
+  @Override
+  public void start(Stage primaryStage) {
+    primaryStage.setTitle("Farming Simulator");
+    Button btn = new Button();
+    btn.setText("Press Button to enter");
+    btn.setOnAction(new EventHandler<ActionEvent>() {
 
+      @Override
+      public void handle(ActionEvent event) {
+        System.out.println("Farming Simulator");
+      }
+    });
 
+    StackPane root = new StackPane();
+    root.getChildren().add(btn);
+    primaryStage.setScene(new Scene(root, 300, 250));
+    primaryStage.show();
+  }
 }
